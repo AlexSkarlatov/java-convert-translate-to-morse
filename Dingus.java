3b87953f5a27e7e7816abf9e318cdbc4993a29d0
AlexSkarlatov <59266442+AlexSkarlatov@users.noreply.github.com>
2020-03-28 19:56:51 -0400

    Add files via upload
    I know its a stupid name for a file, I had a lot going on today and i was kinda just trying to set this thing up from scratch for the third time in  a row. this will be the basis logic for the morse translater
    simple now that the environment is working .... i hope

---
+++ Dingus.java
@@ -0,0 +1,31 @@
+package java_exersise;
+
+import java.util.HashMap;
+
+//import javafx.*;
+//import javafx.application.Application;
+//import javafx.stage.Stage;
+
+
+/*
+ * I know i gave this a stupid name but that was more out of recent
+ * frustration with setting this up in general but the purpose of this is just a basic
+ * exersise in translation of morse code to english and then encrypting
+ * it with a basic ceesar cypher i want this project to be able to do all of that 
+ * so right now this silly .java file will serve as the base for all the basic logic as a sort of testing ground and impl
+ * implementing it cleanly will be the job of the other file  in this class
+ */
+
+public class Dingus {
+	public static void main(String[] args) {
+		System.out.println("helloworld");
+		
+		HashMap<String,String> hm = new HashMap<>();
+	
+	
+	
+	
+	}
+	
+	
+}
